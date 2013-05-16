84ff3cf96fb652af33da3916f441143fd212c05f
Max Malin <malin.mw@gmail.com>
2013-05-16 02:50:56 -0700

    stringifyJSON passes for all 'validObjects'

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,31 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+
+	if (typeof obj === 'number') {
+		return (obj.toString());
+	} else if (typeof obj === 'boolean') {
+		return (obj.toString());
+	} else if (typeof obj === 'string') {
+		return ('"' + obj + '"');
+	} else if (obj === null) {
+		return ('' + obj);
+	} else if (Array.isArray(obj)) {
+		var newArray = [];
+		
+		for (var i = 0; i < obj.length; i++) {
+			newArray.push(stringifyJSON(obj[i]));
+		}
+		return ('[' + newArray + ']');
+	} else {
+		var objectString = '';
+
+		for (var key in obj) {
+			if (objectString !== '') {
+				objectString = objectString + ',';
+			}
+			objectString = objectString + stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+		}
+		return ('{' + objectString + '}');
+	}
 };
